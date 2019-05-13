9022018b59f801c7fefe536654fb60e626fda8af
user <user@LAPTOP-KDBJLAUF>
2019-05-13 13:36:08 +0900

    fourth commit

--- src/main/java/edu/handong/csee/java/inheritance/Cat.java
+++ src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -13,13 +13,13 @@ public class Cat extends Animal {
 	public static void main(String[] args) {
 		Cat myCat = new Cat();
 		
-		myCat.testClassMethod(); 		// hiding 
+		myCat.testClassMethod(); 		// hiding
 		myCat.testInstanceMethod();  	// overriding
-		System.out.println(myCat.name);	// hiding 
+		System.out.println(myCat.name);	// hiding
 		
 		Dog myDog = new Dog();
 		
-		Animal dog = myDog;				// polymorphism- 
+		Animal dog = myDog;				// polymorphism
 		dog.testClassMethod();			// hiding
 		dog.testInstanceMethod();		// overriding 
 		System.out.println(dog.name);	// hiding 
