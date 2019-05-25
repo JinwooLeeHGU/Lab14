29a863813ecf29e5adac219e18efeec966d798d4
user <21400603@handong.edu>
2019-05-25 16:40:10 +0900

    Daily Jandi

--- src/main/java/edu/handong/csee/java/inheritance/Dog.java
+++ src/main/java/edu/handong/csee/java/inheritance/Dog.java
@@ -5,7 +5,7 @@ public class Dog extends Animal {
 	@Override  
 	public void testInstanceMethod() {
 		// TODO Auto-generated method stub
-		//super.testInstanceMethod(); 
+		//super.testInstanceMethod();  
 		System.out.println("testInstamnceMethod in Dog");
 	}
 
